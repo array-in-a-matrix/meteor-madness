c445ede760a7df072c07306542c2d1b52e3c7699
Array in a Matrix <git@arrayinamatrix.xyz>
2025-10-04 14:12:31 -0400

    threejs example

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
 import * as THREE from 'three';
 
-console.log("hello");
+// create a scene, a camera and a way to render them
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+
+// set width & hight of canvas
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// create simple box
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+
+function animate() {
+
+	// rotate cube
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  cube.rotation.z += 0.01;
+
+  // render one frame
+	renderer.render( scene, camera );
+}
+
+// render new scene every time the screen is refreshed, ~60Hz
+renderer.setAnimationLoop( animate );
