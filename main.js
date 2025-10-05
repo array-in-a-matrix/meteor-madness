681c800043dea016c3990ee98558637954691387
Array in a Matrix <git@arrayinamatrix.xyz>
2025-10-05 16:30:39 -0400

    replace cube with \'earth\'

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ import {scene, camera, renderer} from './src/scene.js';
 import axis from './src/axis.js';
 
 // camera vector
-camera.position.set(1, 1, 1);
+camera.position.set(5, 5, 5);
 camera.lookAt(0, 0, 0);
 
 // world axis
 scene.add(axis.x, axis.y, axis.z);
 
-// create simple unit cube centered at the origin
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
+// add earth
+const geometry = new THREE.SphereGeometry(1); 
+const material = new THREE.MeshBasicMaterial( { color: 0x2828B5 } ); 
+const earth = new THREE.Mesh( geometry, material );
+scene.add( earth );
 
 /* TODO:
 - fetch data from NASA APIs then map them
@@ -27,16 +27,9 @@ https://data.nasa.gov/dataset/near-earth-comets-orbital-elements-api/resource/6d
 */
 
 
-let y = 0;
 function animate() {
 
-  // rotate cube
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  cube.rotation.z += 0.01;
 
-  cube.position.set(0,Math.sin(y),0)
-  y=y+0.02
 
   // render one frame
   renderer.render( scene, camera );
