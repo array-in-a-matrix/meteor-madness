4145dee4e7aff1f39b3efc3edaa364baca506691
Array in a Matrix <git@arrayinamatrix.xyz>
2025-10-05 17:46:20 -0400

    fetch from NEO api

--- main.js
+++ main.js
@@ -25,9 +25,26 @@ to different objects in the scene.
 
 - draw orbit path of comets?
 https://data.nasa.gov/dataset/near-earth-comets-orbital-elements-api/resource/6d556122-8052-431d-b8ef-add27e182536
-
-- remove stinky green cube
 */
+
+const startDate = '2025-10-04';
+const endDate = '2025-10-11';
+const asteroidID = '3542519';
+
+const NEOFeedURL = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${KEYS.NEO}`;
+const NEOLookupURL = `https://api.nasa.gov/neo/rest/v1/neo/${asteroidID}?api_key=${KEYS.NEO}`;
+const NEOBrowseURL = `https://api.nasa.gov/neo/rest/v1/neo/browse/?api_key=${KEYS.NEO}`;
+
+
+
+fetch(NEOFeedURL).then(resp => resp.json()).then(resp => {
+	//using the NEO feed API endpoint
+	//print to the console the name of the first object from today's list of NEO
+	console.log(resp.near_earth_objects[startDate][0].name)
+
+	//TODO:
+	// create an object in the scene for each element fetched from the API
+})
 
 
 function animate() {
